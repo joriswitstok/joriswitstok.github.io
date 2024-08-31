9e46fd4cfe81569a993af53bd8d4c17d901110ad
Joris Witstok <jnw30@cam.ac.uk>
2024-08-31 18:01:39 +0100

    Remove favicon from footer

--- src/navigation.js
+++ src/navigation.js
@@ -85,7 +85,6 @@ export const footerData = {
     { ariaLabel: 'X', icon: 'tabler:brand-x', href: 'https://x.com/JorisWitstok', target: '_blank' },
   ],
   footNote: `
-    <img class="w-5 h-5 md:w-6 md:h-6 md:-mt-0.5 bg-cover mr-1.5 rtl:mr-0 rtl:ml-1.5 float-left rtl:float-right rounded-sm" src="./src/assets/favicons/favicon.svg" alt="JNW logo" loading="lazy"></img>
     © ${currentYear} <a href=${getHomePermalink()}> Joris Witstok </a> · <a href="https://github.com/onwidget/astrowind/blob/main/LICENSE.md" target="_blank"> All rights reserved</a>.
   `,
 };
