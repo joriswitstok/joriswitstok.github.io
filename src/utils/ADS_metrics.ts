34b219d3bb71367c027bfd39ae82f664eb37269e
joriswitstok <26546145+joriswitstok@users.noreply.github.com>
2025-11-18 01:14:13 +0000

    Update ADS record

--- src/utils/ADS_metrics.ts
+++ src/utils/ADS_metrics.ts
@@ -1,7 +1,7 @@
-export const latest_update = '17 November, 2025';
+export const latest_update = '18 November, 2025';
 export const n_papers_tot = 118;
-export const n_papers_ref = 90;
-export const n_reads = 125604;
-export const n_downloads = 65446;
-export const n_citations = 10130;
+export const n_papers_ref = 91;
+export const n_reads = 125729;
+export const n_downloads = 65510;
+export const n_citations = 10208;
 export const h_index = 55;
