dc16a5797e36e3bd11c04fd48e898d4d0c15e3c9
joriswitstok <26546145+joriswitstok@users.noreply.github.com>
2026-07-29 02:08:19 +0000

    Update ADS record

--- src/utils/ADS_metrics.ts
+++ src/utils/ADS_metrics.ts
@@ -1,7 +1,7 @@
-export const latest_update = '28 July, 2026';
-export const n_papers_tot = 143;
+export const latest_update = '29 July, 2026';
+export const n_papers_tot = 142;
 export const n_papers_ref = 120;
-export const n_reads = 157109;
-export const n_downloads = 80617;
-export const n_citations = 13530;
+export const n_reads = 157149;
+export const n_downloads = 80628;
+export const n_citations = 13521;
 export const h_index = 64;
